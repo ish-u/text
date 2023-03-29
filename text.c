e53532c400cf38d92a15849ef812d8a48cfc08e5
Anmol Gupta <anmolgupta520@gmail.com>
2023-03-29 23:09:35 +0530

    added PageUp and PageDown support

--- text.c
+++ text.c
@@ -14,7 +14,14 @@
 // Ctrl + Q = 0x17 => 0b01110001 & 0b00011111 = 0b00010001 = 0x17
 #define CTRL_KEY(k) ((k)&0x1f)
 
-enum editorKey { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN };
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
+};
 
 /*** data ***/
 struct editorConfig {
@@ -99,16 +106,30 @@ int editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1)
       return '\x1b';
 
-    if (seq[0]) {
-      switch (seq[1]) {
-      case 'A':
-        return ARROW_UP;
-      case 'B':
-        return ARROW_DOWN;
-      case 'C':
-        return ARROW_RIGHT;
-      case 'D':
-        return ARROW_LEFT;
+    if (seq[0] == '[') {
+      // PageUp - <esc>[5~ , PageDown - <esc>[6~
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+        case 'A':
+          return ARROW_UP;
+        case 'B':
+          return ARROW_DOWN;
+        case 'C':
+          return ARROW_RIGHT;
+        case 'D':
+          return ARROW_LEFT;
+        }
       }
     }
     return '\x1b';
@@ -242,6 +263,13 @@ void editorProcessKeypresses() {
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
+  case PAGE_UP:
+  case PAGE_DOWN: {
+    int times = E.screenrows;
+    while (times--) {
+      editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+  } break;
   case ARROW_UP:
   case ARROW_DOWN:
   case ARROW_LEFT:
