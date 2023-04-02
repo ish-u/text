f21185df2418631c9b19958863e5d4ec32eddca1
Anmol Gupta <anmolgupta520@gmail.com>
2023-04-02 11:40:59 +0530

    cursor motion for lines

--- text.c
+++ text.c
@@ -312,10 +312,22 @@ void editorMoveCursor(int key) {
     if (E.cx != 0) {
       E.cx--;
     }
+    // Move Cursor to end of above line
+    // when pressing left arrow at the start of a line
+    else if (E.cy > 0) {
+      E.cy--;
+      E.cx = E.row[E.cy].size;
+    }
     break;
   case ARROW_RIGHT:
     if (row && E.cx < row->size) {
       E.cx++;
+    }
+    // Move Cursor to start of below line
+    // when pressing right arrow at the end of a line
+    else if (row && E.cx == row->size) {
+      E.cy++;
+      E.cx = 0;
     }
     break;
   case ARROW_UP:
@@ -335,7 +347,7 @@ void editorMoveCursor(int key) {
   // (Snap Cursor to end of line)
   row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   int rowlen = row ? row->size : 0;
-  if(E.cx > rowlen){
+  if (E.cx > rowlen) {
     E.cx = rowlen;
   }
 }
